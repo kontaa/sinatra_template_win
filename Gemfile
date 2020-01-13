f8c013a7cd0f6af4e3d04b4ca335e9525d4f2161
kontaa <gitkontaa@gmail.com>
2020-01-13 23:02:20 +0900

    # on the way #

--- Gemfile
+++ Gemfile
@@ -5,3 +5,7 @@ source "https://rubygems.org"
 gem "sinatra"
 gem "sinatra-contrib"
 
+gem "slim"
+gem "sass"
+gem "coffee-script"
+
--- app.rb
+++ app.rb
@@ -2,11 +2,18 @@ require 'pp'
 require 'bundler/setup'
 require 'sinatra/base'
 require 'sinatra/reloader'
+require 'slim'
+require 'sass'
+require 'coffee-script'
 
 class WebApp < Sinatra::Base
 
   #set :environment, :production
   #warn ENV.select{|k, v| k =~ /env/i}.inspect
+
+  configure :development do
+    register Sinatra::Reloader
+  end
 
   get '/' do
     #warn settings.environment
@@ -16,5 +23,22 @@ class WebApp < Sinatra::Base
 
     "/ " + Time.now.to_s
   end
+
+  get '/index_slim' do
+    slim :index_slim
+  end
+
+  get '/css/*.css' do |file|
+    path = File.join("scss", file)
+    pass unless File.exist? (path + ".scss")
+    scss path.to_sym
+  end
+
+  get '/js/*.js' do |file|
+    path = File.join("coffee", file)
+    pass unless File.exist? (path + ".coffee")
+    coffee path.to_sym
+  end
+
 end
 
